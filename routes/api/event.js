e1b61aca9467b4ed61928bd9ac681e2e0a956df9
Benyakhou El Hadj Larbi <100169315+la26092002@users.noreply.github.com>
2023-11-01 12:40:59 +0100

    Update event.js

--- routes/api/event.js
+++ routes/api/event.js
@@ -82,6 +82,128 @@ router.get('/:id_event',async (req, res) => {
 });
 
 
+//@route    POST api/event/
+//@desc     Add subscribe to Event
+//@access   Private
+router.get('/Subscribe/:id_event',
+    [
+        auth
+    ], async (req, res) => {
+
+        try {
+            const event = await Event.findById(req.params.id_event);
+
+            if (!event) {
+                return res.status(404).json({ msg: 'event not found' });
+            }
+            //req.user.id
+            const user = req.user.id;
+
+            console.log(event.subscribe.length)
+            for (let i = 0; i < event.subscribe.length; i++) {
+                console.log(event.subscribe[i]._id)
+                if (event.subscribe[i]._id == user) {
+                    return res.status(200).json({
+                        msg: 'the user is joined',
+                        status: true
+                    });
+                }
+            }
+            //unshift() the same like push
+
+
+            res.json({
+                msg: 'the user is not joined',
+                status: false
+            });
+        } catch (err) {
+            console.error(err.message)
+            res.status(500).send('server Error');
+        }
+    });
+
+
+//@route    POST api/event/
+//@desc     Add subscribe to Event
+//@access   Private
+router.post('/AddSubscribe/:id_event',
+    [
+        auth
+    ], async (req, res) => {
+
+        try {
+            const event = await Event.findById(req.params.id_event);
+
+            if (!event) {
+                return res.status(404).json({ msg: 'event not found' });
+            }
+            //req.user.id
+            const user = req.user.id;
+
+            console.log(event.subscribe.length)
+            for (let i = 0; i < event.subscribe.length; i++) {
+                console.log(event.subscribe[i]._id)
+                if (event.subscribe[i]._id == user) {
+                    return res.status(200).json({ msg: 'the user is joined' });
+                }
+            }
+            //unshift() the same like push
+            event.subscribe.unshift(user);
+            await event.save();
+
+            res.json(event);
+        } catch (err) {
+            console.error(err.message)
+            res.status(500).send('server Error');
+        }
+    });
+
+
+
+
+//@route    POST api/event/
+//@desc     Add subscribe to Event
+//@access   Private
+router.delete('/RemoveSubscribe/:id_event',
+    [
+        auth
+    ], async (req, res) => {
+        try {
+            const event = await Event.findById(req.params.id_event);
+            if (!event) {
+                return res.status(404).json({ msg: 'event not found' });
+            }
+            //req.user.id
+            const user = req.user.id;
+            console.log(event.subscribe.length)
+            
+            let j=0;
+            for (let i = 0; i < event.subscribe.length; i++) {
+                console.log(event.subscribe[i]._id)
+                if (event.subscribe[i].id === user) {
+                    j++;
+                }
+            }
+
+            if(j===0){
+                return res.status(200).json({ msg: 'the user is not joined' });
+            }
+            const removeIndex = event.subscribe
+                .map((item) => item.id)
+                .indexOf(user);
+            console.log(removeIndex)
+
+            event.subscribe.splice(removeIndex, 1);
+
+            await event.save();
+
+            res.status(200).json(event);
+        } catch (err) {
+            console.error(err.message)
+            res.status(500).send('server Error');
+        }
+    });
+
     
 
 module.exports = router;
